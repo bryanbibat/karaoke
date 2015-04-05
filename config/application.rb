a462fd748cd7c815691267d92f1d27bc0b98dbc5
Bryan Bibat <bry@bryanbibat.net>
2015-04-05 23:55:24 +0800

    Move livereload

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ Bundler.require(*Rails.groups)
 
 module Karaoke
   class Application < Rails::Application
-    config.middleware.use Rack::LiveReload
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
