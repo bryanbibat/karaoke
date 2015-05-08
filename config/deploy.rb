0cb57eeb94cb70fa4c02bc452ffcb60cf7ce2251
Bryan Bibat <bry@bryanbibat.net>
2015-05-08 12:35:58 +0800

    Remove .ruby-version in prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,6 +73,7 @@ task :deploy => :environment do
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      queue "rm #{deploy_to}/#{current_path}/.ruby-version"
     end
   end
 end
