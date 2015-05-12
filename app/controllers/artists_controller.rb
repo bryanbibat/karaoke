289128c33338a4a533eb9ed34afc7d3e5e5934a2
Bryan Bibat <bry@bryanbibat.net>
2015-05-12 23:47:23 +0800

    Add initial pagination to Artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,14 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.order(:name).page params[:page]
+    if params[:initial].present?
+      if ('A'..'Z').include?(params[:initial])
+        @artists = Artist.where("name like '#{params[:initial]}%'").order(:name).page params[:page]
+      else
+        redirect_to songs_path
+      end
+    else
+      @artists = Artist.order(:name).page params[:page]
+    end
   end
 
   def show
