a3e3627a0b340eaa00fa8f56a8c0adf8fcdae125
Bryan Bibat <bry@bryanbibat.net>
2016-01-24 18:54:58 +0800

    Change color of meetup group alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   def j_karaoke_invite
-    flash.now[:notice] = "We've started a karaoke group for people interested in singing Japanese songs! Join us at <a href='http://www.meetup.com/J-Karaoke-Manila/'>http://www.meetup.com/J-Karaoke-Manila/</a>.".html_safe
+    flash.now[:info] = "We've started a karaoke group for people interested in singing Japanese songs! Join us at <a href='http://www.meetup.com/J-Karaoke-Manila/'>http://www.meetup.com/J-Karaoke-Manila/</a>.".html_safe
   end
 end
